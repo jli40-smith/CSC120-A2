abf605f6251b3084b44d32ec38c420eab5d1441a
Zoe Plumridge <zplumzebra@gmail.com>
2022-09-15 22:22:11 -0400

    Zoe's attempt

--- computer.py
+++ computer.py
@@ -1,25 +1,25 @@
-# What attributes will it need?
+class Computer:
+
+    # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: str
+    memory: str
+    operating_system: str
+    year_made: str
+    price: str
 
     # How will you set up your constructor?
+
+
     # Remember: in python, all constructors have the same name (__init__)
-class Computer:
-    #Constructor sets up the class Computer
-    def __init__(self, description: str,
-                    processor_type: str,
-                    hard_drive_capacity: int,
-                    memory: int,
-                    operating_system: str,
-                    year_made: int,
-                    price: int):
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
         self.description = description
         self.processor_type = processor_type
-        self.hard_drive = hard_drive_capacity
+        self.hard_drive_capacity = hard_drive_capacity
         self.memory = memory
         self.operating_system = operating_system
         self.year_made = year_made
-        self.price = price
+        
 
-x = Computer("MacBook Pro","Intel", 256, 16,"Mac OS", 2017, 1999)
-
-print(x.description, x.processor_type, x.hard_drive, x.memory, x.operating_system, x.year_made, x.price)
     # What methods will you need?
